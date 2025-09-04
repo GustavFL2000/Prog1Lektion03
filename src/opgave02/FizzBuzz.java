59f29402975f9a265d9367fe1e3aa30396b827b0
LumreLars <gustavfl13@gmail.com>
2025-09-04 15:08:18 +0200

    Opgave 2

--- src/opgave02/FizzBuzz.java
+++ src/opgave02/FizzBuzz.java
@@ -4,8 +4,12 @@ public class FizzBuzz {
     public static void main(String[] args) {
         int currentNumber = 1;
         while (currentNumber <= 40) {
-            //Indsæt din implementering af FizzBuzz her.
+            if(currentNumber % 3 == 0){System.out.println("Fizz");}
+            else if(currentNumber % 5 == 0){System.out.println("Buzz");}
+            else if(currentNumber % 3 == 0 && currentNumber % 5 == 0){System.out.println("FizzBuzz");}
+            else {System.out.println(currentNumber);}
             currentNumber++;
+            
         }
     }
 }
