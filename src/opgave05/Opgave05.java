e1c864c9ade03c4be7a5edf6521b2026724d33ba
LumreLars <gustavfl13@gmail.com>
2025-09-08 10:56:23 +0200

    Opgave 5 tests virker

--- src/opgave05/Opgave05.java
+++ src/opgave05/Opgave05.java
@@ -13,7 +13,24 @@ public class Opgave05 {
     }
 
     public static double calculateFee(int price) {
+        double fee = 0;
 
-        return 15000;
+        if (price <= 67800) {
+            fee = price * 0.25;
+        } else if (price <= 210600) {
+            // Første 67.800 kr. = 25%
+            fee = 67800 * 0.25;
+            // Resten = 85%
+            fee += (price - 67800) * 0.85;
+        } else {
+            // Første 67.800 kr. = 25%
+            fee = 67800 * 0.25;
+            // 67.800 – 210.600 kr. = 85%
+            fee += (210600 - 67800) * 0.85;
+            // Over 210.600 kr. = 150%
+            fee += (price - 210600) * 1.5;
+        }
+
+        return fee;
     }
 }
