3344a6033cfb2832b7c72eff25bf3ab2fdbb15fb
LumreLars <gustavfl13@gmail.com>
2025-09-08 10:47:08 +0200

    ændringer i readme + lidt opgaver

--- src/opgave03/Opgave03.java
+++ src/opgave03/Opgave03.java
@@ -1,6 +1,37 @@
 package opgave03;
-
+import java.util.Scanner;
 public class Opgave03 {
+
+    public static String weekDay(int dag){
+        if (dag == 0){
+            return "Monday";
+        } else if(dag == 1){
+            return "Tuesday";
+        } else if(dag == 2){
+            return "Wednesday";
+        } else if(dag == 3){
+            return "Thursday";
+        } else if(dag == 4){
+            return "Friday";
+        } else if(dag == 5){
+            return "Saturday";
+        } else if(dag == 6){
+            return "Sunday";
+        }
+        return "That is not a valid day";
+    }
     public static void main(String[] args) {
+        /*for (int i = 0; i < 7; i++) {
+            System.out.println(weekDay(i));
+        }*/
+        while (true) {
+            
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number (0-6): ");
+        int day = scanner.nextInt();
+
+        System.out.println(weekDay(day));
+        }
     }
 }
--- src/opgave04/Opgave04.java
+++ src/opgave04/Opgave04.java
@@ -1,4 +1,46 @@
 package opgave04;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Opgave04 {
+
+    public static String game(int hand){
+        switch(hand) {
+    case 0:
+    return "Rock";
+    case 1:
+    return "Paper";
+    case 2:
+    return "Scissors";
+    default:
+    return "That is not a valid hand";
+}
+    }
+    public static void main(String[] args) {
+        while(true){
+        //Spiller vælger hånd
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Vælg din hånd (0=Rock, 1=Paper, 2=Scissors): ");
+        int userHand = scanner.nextInt();
+        System.out.print("Spiller har valgt: ");
+        System.out.println(game(userHand));
+
+        //Computer vælger hånd
+        Random random = new Random();
+        int hand = random.nextInt(3);
+        System.out.print("Computer har valgt: ");
+        System.out.println(game(hand));
+
+        //Bestem vinder
+        if (userHand == hand) {
+            System.out.println("It's a tie!");
+        } else if ((userHand == 0 && hand == 2) || (userHand == 1 && hand == 0) || (userHand == 2 && hand == 1)) {
+            System.out.println("You win!");
+        } else if ((userHand == 0 && hand == 1) || (userHand == 1 && hand == 2) || (userHand == 2 && hand == 0)) {
+            System.out.println("Computer wins!");
+        } else {
+            System.out.println("Invalid input. Please enter 0, 1, or 2.");
+        }
+    }
+    }
 }
